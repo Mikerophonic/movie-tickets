c3ff6023d8d6b08991aace97f1fd30bae6be6e67
Mikerophonic <micgcon@gmail.com>
2023-07-06 11:25:04 -0700

    write ticket constructor and calculate Price function

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+function Ticket(movieName, timeOfDay, age) {
+    this.movieName = movieName;
+    this.timeOfDay = timeOfDay;
+    this.age = age;
+}
+
+Ticket.prototype.calculatePrice() {
+    let basePrice = 12;
+    if (movieName !== "first-release") {
+        basePrice -= 2;
+    }
+    if (timeOfDay === "matinee") {
+        basePrice -= 3;
+    }
+    if (age <= 60) {
+        basePrice -= 4;
+    }
+    return basePrice;
+}
