4234e0ee0d69ee1aebbb638ba7f42c0c5042c129
Mikerophonic <micgcon@gmail.com>
2023-07-06 12:05:16 -0700

    add UI logic

--- js/scripts.js
+++ js/scripts.js
@@ -6,14 +6,33 @@ function Ticket(movieName, timeOfDay, age) {
 
 Ticket.prototype.calculatePrice = function() {
     let basePrice = 12;
-    if (movieName !== "first-release") {
+    if (this.movieName !== "first-release") {
         basePrice -= 2;
     }
-    if (timeOfDay === "matinee") {
+    if (this.timeOfDay === "matinee") {
         basePrice -= 3;
     }
-    if (age <= 60) {
+    if (this.age >= 60) {
         basePrice -= 4;
     }
     return basePrice;
 }
+
+
+
+//UI Logic
+
+function handleFormSubmission(event) {
+    event.preventDefault();
+    const inputtedMovieName = document.getElementById("movie-select").value;
+    const inputtedTimeOfDay = document.querySelector('input[name="time"]:checked').value;
+    const inputtedAge = document.getElementById("age").value;
+    const ticket = new Ticket(inputtedMovieName, inputtedTimeOfDay, inputtedAge);
+    const ticketPrice = ticket.calculatePrice();
+    const totalDiv = document.getElementById("total");
+    totalDiv.innerHTML = `$${ticketPrice}`;
+}
+
+window.addEventListener("load", function (){
+    document.querySelector("button").addEventListener("click", handleFormSubmission);
+});
